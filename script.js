c34148815b71c58984cf48cd0b5bd889799ec32f
Faizul <faizulmd1306@gmail.com>
2025-08-24 19:25:35 +0530

    add checkwinner and refactor to use with UI

--- script.js
+++ script.js
@@ -28,15 +28,78 @@ const GameBoard = (() => {
     }
   };
 
-  return { getBoard, placeMark, resetBoard };
+  const isTie = () => {
+    return !board.includes("") && !checkWinner();
+  };
+
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8], // rows
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8], // cols
+    [0, 4, 8],
+    [2, 4, 6], // diagonals
+  ];
+
+  const checkWinner = () => {
+    for (const [a, b, c] of winningCombinations) {
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return board[a];
+      }
+    }
+    return null;
+  };
+
+  return { getBoard, placeMark, resetBoard, checkWinner, isTie };
 })();
 
 const Player = (name, marker) => {
-  const value = marker === "X" ? 1 : 2;
-
   const getName = () => name;
   const getMarker = () => marker;
-  const getValue = () => value;
 
-  return { getName, getMarker, getValue };
+  return { getName, getMarker };
 };
+
+const GameController = (() => {
+  const playerOne = Player("Faizul", "X");
+  const playerTwo = Player("Kishore", "O");
+
+  //   const firstPlayer = () => {
+  //     if (playerOne.getMarker() === "X") {
+  //       return playerOne;
+  //     } else {
+  //       return playerTwo;
+  //     }
+  //   };
+
+  let currentPlayer = playerOne;
+
+  const switchTurn = () => {
+    currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
+  };
+
+  const playRound = (index) => {
+    if (GameBoard.placeMark(index, currentPlayer.getMarker())) {
+      const winner = GameBoard.checkWinner();
+      if (winner) {
+        const winningPlayer =
+          winner === playerOne.getMarker() ? playerOne : playerTwo;
+        return { status: "win", message: `${winningPlayer.getName()} wins!` };
+      }
+
+      if (GameBoard.isTie()) {
+        return { status: "tie", message: "It's a tie!" };
+      }
+
+      switchTurn();
+      return { status: "next", player: getCurrentPlayer().getName() };
+    }
+    return { status: "invalid", message: "Cell already taken!" };
+  };
+
+  const getCurrentPlayer = () => currentPlayer;
+
+  return { playRound, getCurrentPlayer, reset: GameBoard.resetBoard };
+})();
